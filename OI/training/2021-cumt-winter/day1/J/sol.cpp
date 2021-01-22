a484a7fda5f880fbd83fbabd89814ddfc9a0ba26
cumt10 <cumt10@iMac-8.local>
2021-01-22 16:52:51 +0800

    debugging

--- OI/training/2021-cumt-winter/day1/J/sol.cpp
+++ OI/training/2021-cumt-winter/day1/J/sol.cpp
@@ -12,6 +12,7 @@
 #include <vector>
 #include <string>
 #include <map>
+#include <unordered_map>
 #define rep(i,a,b) for(int i=(a);i<=(b);++i)
 #define per(i,a,b) for(int i=(a);i>=(b);--i)
 #define repp(i,a,b) for(int i=(a);i<(b);++i)
@@ -29,39 +30,6 @@ typedef vector<int> VI;
 typedef vector<ll> VL;
 typedef vector<ldb> VD;
 
-template<class T> inline void read(T &x) {
-    x = 0; char ch;
-    do{ch=getchar();} while(ch<'0'||ch>'9');
-    do{x=(x<<3)+(x<<1)+(x&15),ch=getchar();} while(ch>='0'&&ch<='9');
-}
-int Mod = 1;
-inline int Inc(int x, int y) { return (x += y) < Mod ? x : x - Mod; }
-inline int Dec(int x, int y) { return (x -= y) < 0 ? x + Mod : x; }
-inline int Mul(int x, int y) { return 1ll * x * y % Mod; }
-inline int Power(int x, int y) {
-    int ret = 1%Mod;
-    for(; y; y>>=1) {
-        if(y&1) ret=(ll)ret*x%Mod;
-        x=(ll)x*x%Mod;
-    }
-    return ret;
-}
-
-template<class T> inline T abs(T x) { return x > 0 ? x : -x; }
-template<class T> inline void cmin(T &x, T &y) { x = x > y ? y : x; }
-template<class T> inline void cmax(T &x, T &y) { x = x > y ? x : y; }
-template<class T> inline T min(T x, T y) { return x < y ? x : y; }
-template<class T> inline T max(T x, T y) { return x > y ? x : y; }
-
-template<class T>
-class BIT { public:
-    const int maxN;
-    vector<T> c;
-    BIT(int size) : maxN(size) { c.rz(size + 1); }
-    void add(int x, T v) { for(; x <= maxN; x += x&(-x)) c[x] += v; }
-    T qry(int x) { T ret = 0; for(; x; x -= x&(-x)) ret += c[x]; return ret; }
-};
-
 inline int genpls(string s) {
     for(int i = 0; i < 9; ++i) if(s[i] == 'X') return i;
     return -1;
@@ -69,28 +37,86 @@ inline int genpls(string s) {
 
 const int d[] = {3, -1, 1, -3};
 const string ts[] = {"d", "l", "r", "u"};
+unordered_map<int, int> ans[10];
 
-queue<string> q;
-map<string, string> exi;
-void bfs(string s, string t) {
-    while(!q.empty()) q.pop();
-    q.push(s);
-    exi[s] = "";
-    if(s == t) return;
+// inline int toint(int *a) {
+//     int ret = 0;
+//     rep(i,0,8) ret = ret * 10 + a[i];
+//     return ret;
+// }
+
+// inline int tovec(int *v, int num) {
+//     int ret = 0;
+//     per(i,8,0) {
+//         v[i] = num % 10;
+//         num /= 10;
+//         if(v[i] == 0) ret = i;
+//     }
+//     return ret;
+// }
+
+int fac[10];
+
+inline int tovec(int *v, int num) {
+    int ret = -1;
+    rep(i,0,8) {
+        v[i] = num % fac[8-i] - 1;
+        num /= fac[8-i];
+        if(v[i] == 0) ret = i;
+    }
+    rep(i,0,8) cout << v[i]; cout << ' ';
+    cout << num << "#\n"; cout << endl;
+    cout.flush();
+    return ret;
+}
+
+inline int toint( int *v ) {
+    int used[9] = {0}, ret = 0;
+    rep(i,0,8) {
+        int t = v[i];
+        repp(i,0,v[i]) t -= used[i];
+        ret += t * fac[8-i];
+        used[v[i]] = 1;
+        cout << ret << endl;
+    }
+    cout << "#" << ret << ' ';
+    rep(i,0,8) cout << v[i]; cout << endl;
+    cout.flush();
+    return ret;
+}
+
+queue<int> q;
+int counter = 0;
+void bfs(int *svec, int pls) {
+    unordered_map<int,int>& mp = ans[pls];
+    int sint = toint(svec);
+    q.push(sint); mp[sint] = -1;
+    int curvec[9];
     while(!q.empty()) {
-        string cur = q.front(); q.pop();
-        int pls = genpls(cur);
-        for(int i = 0; i < 4; ++i) {
+        int curint = q.front(); q.pop();
+        int pls = tovec(curvec, curint);
+        repp(i,0,4) {
             int to = pls + d[i];
             if(to > -1 && to < 9) {
-                string nex(cur); swap(nex[pls], nex[to]);
-                if(exi.find(nex) == exi.end()) {
-                    exi[nex] = exi[cur] + ts[i];
-                    q.push(nex);
-                    if(nex == t) return;
+                swap(curvec[to], curvec[pls]);
+                int nexint = toint(curvec);
+                if(mp.find(nexint) == mp.end()) {
+                    q.push(nexint);
+                    mp[nexint] = i;
                 }
+                swap(curvec[to], curvec[pls]);
             }
         }
+    }
+
+}
+
+void prework() {
+    int a[9] = {0, 1, 2, 3, 4, 5, 6, 7, 8};
+    bfs(a, 0);
+    rep(i,0,7) {
+        swap(a[i], a[i+1]);
+        bfs(a, i+1);
     }
 }
 
@@ -101,15 +127,38 @@ int main() {
     }
     ios::sync_with_stdio(false);
     cin.tie(NULL);
+        fac[0] = fac[1] = 1;
+    rep(i,2,9) fac[i] = fac[i-1] * i;
+    prework();
     int tests = 1;
     cin >> tests;
+    char str[10];
+    int svec[9], tvec[9], revec[15];
+    string answer;
     rep(test,1,tests) {
-        string s, t;
-        cin >> s >> t;
-        bfs(s, t);
-        string ans(exi[t]);
-        cout << "Case " << test << ": " << ans.length() << '\n' << ans << '\n';
-
+        memset(revec, 0, sizeof(revec));
+        cin >> str;
+        int typ, cnter = 0;
+        rep(i,0,8) {
+            if(str[i] == 'X') svec[i] = 0, typ = i;
+            else svec[i] = str[i] - '0', revec[svec[i]] = ++cnter, svec[i] = cnter;
+        }
+        cin >> str;
+        rep(i,0,8) {
+            if(str[i] == 'X') tvec[i] = 0;
+            else tvec[i] = str[i] - '0';
+            tvec[i] = revec[tvec[i]];
+        }
+        int cur = toint(tvec), sint = toint(svec);
+        int curvec[9];
+        answer.clear();
+        while(cur != sint) {
+            answer = ts[ans[typ][cur]] + answer;
+            int pls = tovec(curvec, cur);
+            swap(curvec[pls], curvec[-d[ans[typ][cur]] + pls]);
+            cur = toint(curvec);
+        }
+        cout << "Case " << test << ": " << answer.length() << '\n' << answer << '\n';
     }
     return 0;
 }
